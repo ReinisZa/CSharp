193a0dfdd95bf7f1d61ef24f593438f3623807d9
ReinisZa <70597368+ReinisZa@users.noreply.github.com>
2020-09-04 19:20:23 +0300

    Added Day05, Calculator created

---
+++ Day05/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day05
+{
+    class Calculator
+    {
+        public static double Sum(double a, double b)
+        {
+            double sum = a + b;
+            return sum;
+        }
+        public static double Sub(double a, double b)
+        {
+            double sum = a - b;
+            return sum;
+        }
+        public static double Time(double a, double b)
+        {
+            double sum = a * b;
+            return sum;
+        }
+        public static double Div(double a, double b)
+        {
+            double sum = a / b;
+            return sum;
+        }
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Choose one - sum, sub, time, div: ");
+            String choice = Console.ReadLine();
+
+            Console.Write("Choose number: ");
+            double a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Choose number: ");
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            if(choice == "sum")
+            {
+                Console.WriteLine(Calculator.Sum(a, b));
+            }
+            else if(choice == "sub")
+            {
+                Console.WriteLine(Calculator.Sub(a, b));
+            }
+            else if (choice == "div")
+            {
+                Console.WriteLine(Calculator.Div(a, b));
+            }
+            else if (choice == "time")
+            {
+                Console.WriteLine(Calculator.Time(a, b));
+            }
+        }
+    }
+}
