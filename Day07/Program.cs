1ce3ed47aa6940439c4c3ca1397347c77cf10ecf
ReinisZa <70597368+ReinisZa@users.noreply.github.com>
2020-09-08 21:16:49 +0300

    Created List with Class students

---
+++ Day07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task01();
+        }
+        static void Task01()
+        {
+            List<Students> StudentsLst = new List<Students>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter student name: ");
+                String name = Console.ReadLine();
+
+                Console.Write("Enter student surname: ");
+                String surname = Console.ReadLine();
+
+                Console.Write("Enter student course: ");
+                int course = Convert.ToInt32(Console.ReadLine());
+
+                StudentsLst.Add(new Students(name, surname, course));
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+            {
+                StudentsLst[i].PrintInfo();
+            }
+        }
+    }
+}
---
+++ Day07/Students.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day07
+{
+    class Students
+    {
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        private int _course;
+        public int Course
+        {
+            get
+            {
+                return _course;
+            }
+            set
+            {
+                _course = value;
+
+                if (_course < 1)
+                {
+                    _course = 1;
+                }    
+                else if (_course > 3)
+                {
+                    _course = 3;
+                }
+
+            }
+        }
+
+        public Students(String name, String surname, int course)
+        {
+            Name = name;
+            Surname = surname;
+            Course = course;
+
+        }
+
+        /*public Students()
+        {
+            Console.Write("Enter name: ");
+            String name = Console.ReadLine();
+            Name = name;
+            Console.Write("Enter surname: ");
+            String surname = Console.ReadLine();
+            Surname = surname;
+            Console.Write("Enter Course: ");
+            int course = Convert.ToInt32(Console.ReadLine());
+            Course = course;
+        }*/
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(Name + " " + Surname + " " + Course);
+        }
+    }
+}
