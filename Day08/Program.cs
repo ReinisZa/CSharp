2221be0df25748dfbaeb2b40f5490cef5caae89b
ReinisZa <70597368+ReinisZa@users.noreply.github.com>
2020-09-10 19:50:04 +0300

    Created Rectangle calculator

---
+++ Day08/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Taisnsturis> TSturis = new List<Taisnsturis>();
+            int hgt = 0;
+            int lgh = 0;
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.Write("Enter height [ " + i + " ]: ");
+                try
+                {
+                    hgt = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    hgt = -1;
+                }
+
+                Console.Write("Enter length [ " + i + " ]: ");
+                try
+                {
+                    lgh = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    lgh = -1;
+                }
+
+                if (lgh < 0 || hgt < 0)
+                {
+                    Console.WriteLine("Wrong input.");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    TSturis.Add(new Taisnsturis(hgt, lgh));
+                    Console.WriteLine();
+                }
+            }
+
+            for(int i = 0; i < TSturis.Count; i++)
+            {
+                Console.Write(i + " Rectangle "); TSturis[i].PrintInfo();
+            }
+        }
+    }
+}
---
+++ Day08/Taisnsturis.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day08
+{
+    class Taisnsturis
+    {
+        int area = 0;
+        public int Hgt { get; set; }
+        public int Lgh { get; set; }
+
+        public int Area 
+        {
+            get
+            {
+                int area = Hgt * Lgh;
+                return area;
+            }
+            set
+            {
+                Area = area;
+            }
+            
+        }
+
+        public Taisnsturis (int hgt, int lgh)
+        {
+            Hgt = hgt;
+            Lgh = lgh;
+            
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(Hgt + " * " + Lgh + " = " + Area + " Area");
+        }
+    }
+}
